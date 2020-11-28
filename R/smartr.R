d58b728f4691563bd1c8402826b48cf58160fb7c
Ernest Guevarra <ernest@guevarra.io>
2020-11-28 22:31:34 +0000

    initial commit

---
+++ R/smartr.R
@@ -0,0 +1,23 @@
+################################################################################
+#
+#'
+#' Tools for Nutritional Status and Mortality Rate Assessments and Surveys
+#'
+#' SMART is an inter-agency initiative launched in 2002 by a network of
+#' organizations and humanitarian practitioners. SMART advocates a
+#' multi-partner, systematized approach to provide critical, reliable
+#' information for decision-making, and to establish shared systems and
+#' resources for host government partners and humanitarian organizations. The
+#' SMART Methodology is an improved survey method that balances simplicity
+#' (for rapid assessment of acute emergencies) and technical soundness. This
+#' package provides functions to perform the various analysis used in SMART.
+#'
+#' @docType package
+#' @name smartr
+#' @keywords internal
+#'
+#
+################################################################################
+"_PACKAGE"
+
+
