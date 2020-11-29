9db2a60e25425a35218e5bccb6e0f69db9093ee6
Ernest Guevarra <ernest@guevarra.io>
2020-11-29 07:03:24 +0000

    document datasets

---
+++ R/data.R
@@ -0,0 +1,25 @@
+################################################################################
+#
+#'
+#' List of villages in Dessie Zuria survey with population sizes and cumulative
+#' population size range
+#'
+#' @format A tibble with 281 rows and 3 columns:
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *village_id* | Village identifier |
+#' | *village_name* | Name of village |
+#' | *pop* | Village population size |
+#' | *cum_pop* | Cumulative population size range |
+#'
+#' @source SMART, Action Against Hunger-Canada, and Technical Advisory Group
+#'
+#' @examples
+#' clusterList
+#'
+#'
+#
+################################################################################
+"clusterList"
+
