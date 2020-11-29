a043db8f9786a20e515631aa97976b2970149261
Ernest Guevarra <ernest@guevarra.io>
2020-11-29 07:03:48 +0000

    create datasets

--- data-raw/process.R
+++ data-raw/process.R
@@ -1,1 +1,16 @@
-##
+## Load libraries
+library(readxl)
+library(dplyr)
+
+## Read cluster assignment data
+clusterList <- read_xlsx(path = "data-raw/smartfiles/Cluster assignment.xlsx",
+                         range = "A3:C284")
+
+village_id <- 1:nrow(clusterList)
+
+clusterList <- tibble::tibble(village_id, clusterList)
+
+names(clusterList) <- c("village_id", "village_name", "pop", "cum_pop")
+
+usethis::use_data(clusterList, overwrite = TRUE, compress = "xz")
+
