1de18daa60f4cb02e1859b5717e5559f382e6936
Ernest Guevarra <ernest@guevarra.io>
2020-12-03 20:32:17 +0000

    create forms functions

---
+++ R/forms.R
@@ -0,0 +1,38 @@
+################################################################################
+#
+#'
+#' Create a Google Sheets data entry form
+#'
+#' @param name A string to name the newly created spreadsheet.
+#' @param sheets Optional input for initialising worksheets.
+#' @paramm view Logical. Should the newly created sheet be opened in the
+#'   current browser? Default to TRUE.
+#'
+#' @return A spreadsheet with specified name and specified sheets
+#'   in user's Google Sheets account.
+#'
+#' @author Ernest Guevarra
+#'
+#' @examples
+#' ## Create a Google Sheet named "village_list"
+#' if (googlesheets4::gs4_has_token()) {
+#'   create_gsheet_form("SMART Survey")
+#' }
+#'
+#' @export
+#'
+#
+################################################################################
+
+create_gsheet_form <- function(name, sheets = NULL) {
+  if (!requireNamespace(package = "googlesheets4", quietly = TRUE)) {
+    stop("The 'googlesheets4' package is required. Install to use this function.",
+         call. = TRUE)
+  }
+
+  name <- names %>%
+    stringr::str_replace_all(pattern = " ", replacement = "_")
+
+  googlesheets4::gs4_create(name = name, sheets = sheets)
+}
+
